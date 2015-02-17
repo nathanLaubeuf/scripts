42abb3d0887cf4f49a0d73f7d73850833d8986e3
nathanLaubeuf <nathan.laubeuf@gmail.com>
2015-02-17 17:06:40 +0100

    eddited workflow for reunify_both_regions

--- workflows.py
+++ workflows.py
@@ -23,6 +23,10 @@ def ReconAll(name='ReconAll'):
 
 
 def Surface(name='surface'):
+    inputnode_rl = pe.Node(interface=niu.IdentityInterface(fields=['rh','lh']), name='inputnode_rl')
+    inputnode_rl.inputs.rh = 'rh'
+    inputnode_rl.inputs.lh = 'lh'
+    inputnode_rl.itterables = ("rl", ['rh','lh'])
     inputnode = pe.Node(interface=niu.IdentityInterface(fields=['pial', 'annot', 'ref_tables','rl']), name='inputnode')
     pial2asc = pe.Node(interface=fs.utils.MRIsConvert(), name='pial2asc')
     pial2asc.inputs.normal = True
@@ -36,36 +40,56 @@ def Surface(name='surface'):
     off2txt = pe.Node(interface=niu.Function(input_names=['surface', 'rl'],
                                              output_names=['vertices_low', 'triangles_low'],
                                              function=su.off2txt), name='off2txt')
-    region_mapping = pe.Node(interface=su.RegionMapping(),name=region_mapping)
-    correct_region_mapping = pe.Node(interface=niu.Function(input_names=[
-        'region_mapping_not_corrected', 'vertices', 'triangles', 'rl', 'region_mapping_corr'], 
-        output_names = ['region_mapping_low'],
-        function=su.correct_region_mapping(),
-        name='correct_region_mapping'))
+    region_mapping = pe.Node(interface=su.RegionMapping(),name='region_mapping')
+    correct_region_mapping = pe.Node(interface=niu.Function(input_names=['region_mapping_not_corrected', 'vertices', 'triangles', 'rl', 'region_mapping_corr'], 
+                                                            output_names = ['region_mapping_low'],
+                                                            function=su.correct_region_mapping),name='correct_region_mapping')
     check_region_mapping = pe.Node(interface=su.CheckRegionMapping(), name='check_region_mapping')
-    reunify_both_regions = pe.Node(interface=su.reunify_both_regions(), name='reunify_both_regions')
-
-    wf = pe.Workflow(name=name)
-    wf.connect([
-        (inputnode, pial2asc,[('pial','in_file')])
-        (inputnode, RegionMapping, [('annot', 'aparc_annot'),('ref_tables','ref_tables'),('rl','rl')]),
+    reunify_both_regions = pe.Node(interface=niu.Function(input_names = ['rh_region_mapping', 'lh_region_mapping', 'rh_vertices', 'lh_vertices', 'rh_triangles', 'lh_triangles'],
+                                                          output_names = ['out_files'],
+                                                          function = su.reunify_both_regions), name='reunify_both_regions')
+    wfrh = pe.Workflow(name=name)
+    wfrh.connect([
+        (inputnode, pial2asc,[('pial','in_file')]),
+        (inputnode, region_mapping, [('annot', 'aparc_annot'),
+                                     ('ref_tables','ref_tables'),
+                                     ('rl','rl')]),
         (inputnode, correct_region_mapping, [('rl','rl')]),
         (inputnode, extract_high,[('rl','rl')]),
         (inputnode, txt2off, [('rl','rl')]),
         (inputnode, off2txt,[('rl','rl')]),
-        (pial2asc, extract_high, [('out_file','surface')]),
-        (extract_high, txt2off, [('vertices_high','vertices'),('triangles_high','triangles')])
+        (pial2asc, extract_high, [('converted','surface')]),
+        (extract_high, txt2off, [('vertices_high','vertices'),
+                                 ('triangles_high','triangles')]),
         (extract_high, region_mapping,[('vertices_high','vertices_high')]),
         (txt2off, remesher, [('out_file','in_file')]),
         (remesher, off2txt, [('out_file','surface')]),
-        (off2txt, region_mapping, [('vertices_low','vertices_low'),('triangles_low','triangles_low')]),
-        (off2txt, correct_region_mapping, [('vertices_low','vertices'),('triangles_low','triangles')]),
-        (off2txt, check_region_mapping, [('vertices_low','vertices_low'),('triangles_low','triangles_low')]),
+        (off2txt, region_mapping, [('vertices_low','vertices_low'),
+                                   ('triangles_low','triangles_low')]),
+        (off2txt, correct_region_mapping, [('vertices_low','vertices'),
+                                           ('triangles_low','triangles')]),
+        (off2txt, check_region_mapping, [('vertices_low','vertices_low'),
+                                         ('triangles_low','triangles_low')]),
         (region_mapping, correct_region_mapping,[('out_file','region_mapping_not_corrected')]),
         (correct_region_mapping, check_region_mapping, [('region_mapping_low','region_mapping_low')])
         ])
-    return wf
 
+    wflh = wfrh.clone(name='wflh')
+
+    wf = pe.Workflow(name=name)
+    wf.connect([
+        (inputnode_rl, wfrh,[('rh','inputnode.rl')]),
+        (inputnode_rl, wflh,[('lh','inputnode.rl')]),
+        (wfrh,reunify_both_regions,[('check_region_mapping.region_mapping_low', 'rh_region_mapping')]),
+        (wflh,reunify_both_regions,[('check_region_mapping.region_mapping_low', 'lh_region_mapping')]),
+        (wfrh,reunify_both_regions,[('off2txt.vertices_low', 'rh_vertices')]),
+        (wflh,reunify_both_regions,[('off2txt.vertices_low', 'lh_vertices_list')]),
+        (wfrh,reunify_both_regions,[('off2txt.triangles_low', 'rh_triangles')]),
+        (wflh,reunify_both_regions,[('off2txt.triangles_low', 'lh_triangles')])
+        ])
+
+
+    return wf
 
 def SubcorticalSurface(name='subcorticalsurfaces'):
     """ extraction of the subcortical surfaces from FreeSurfer"""
